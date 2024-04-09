edee3e200e69fbd8268b2d3ab8d3d26d39af41c3
Nero15275 <ranitdey.rani@gmail.com>
2024-04-09 20:53:41 +0530

    validation changes

--- src/components/contact/Contact.tsx
+++ src/components/contact/Contact.tsx
@@ -4,7 +4,7 @@ import { AiOutlineMail } from 'react-icons/ai'
 import { FaFacebookMessenger } from 'react-icons/fa'
 import { BsWhatsapp } from 'react-icons/bs'
 import emailjs from 'emailjs-com'
-import { ToastContainer, toast } from 'react-toastify';
+import { Bounce, ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
 const Contact = () => {
@@ -14,23 +14,87 @@ const Contact = () => {
         message: ''
     }
     const [contactMessage, setcontactMessage] = useState(initialState)
+    const [errorClass, setErrorClass] = useState('')
     const handleChange = (event: any) => {
+        setErrorClass('')
         setcontactMessage({ ...contactMessage, [event.target.name]: event.target.value })
     }
 
     const onSubmit = (e: any) => {
         e.preventDefault()
+        if (contactMessage.name == '') {
+            setErrorClass('name')
+            toast.error('Please enter your name', {
+                position: "top-right",
+                autoClose: 3000,
+                hideProgressBar: true,
+                closeOnClick: true,
+                pauseOnHover: true,
+                draggable: false,
+                progress: undefined,
+                theme: "colored",
+                transition: Bounce,
+            })
+            return
+        }
+        if (contactMessage.email == '') {
+            setErrorClass('email')
+            toast.error('Please enter your email address', {
+                position: "top-right",
+                autoClose: 3000,
+                hideProgressBar: true,
+                closeOnClick: true,
+                pauseOnHover: true,
+                draggable: false,
+                progress: undefined,
+                theme: "colored",
+                transition: Bounce,
+            })
+            return
+        }
+        if (contactMessage.message == '') {
+            setErrorClass('message')
+            toast.error('Please enter a message', {
+                position: "top-right",
+                autoClose: 3000,
+                hideProgressBar: true,
+                closeOnClick: true,
+                pauseOnHover: true,
+                draggable: false,
+                progress: undefined,
+                theme: "colored",
+                transition: Bounce,
+            })
+            return
+        }
         emailjs.send("service_vdzjy16", "template_vcvojqe", contactMessage, "qsqhVydQURVz-HOxR").then(
             (res) => {
-                toast("easssssyyyyyynpm")
-                toast.success("Mail sent successfully", {
-                    position: toast.POSITION.TOP_CENTER,
-                })
+                if (res.status === 200)
+                    toast.success("Email Sent Successfully !", {
+                        position: "top-right",
+                        autoClose: 3000,
+                        hideProgressBar: true,
+                        closeOnClick: true,
+                        pauseOnHover: true,
+                        draggable: false,
+                        progress: undefined,
+                        theme: "colored",
+                        transition: Bounce,
+                    })
+
                 setcontactMessage(initialState)
 
             }).catch(err => {
                 toast.error("Server Down It will be fixed shortly", {
-                    position: toast.POSITION.TOP_CENTER,
+                    position: "top-right",
+                    autoClose: 3000,
+                    hideProgressBar: true,
+                    closeOnClick: true,
+                    pauseOnHover: true,
+                    draggable: false,
+                    progress: undefined,
+                    theme: "colored",
+                    transition: Bounce,
                 })
             })
     }
@@ -60,9 +124,9 @@ const Contact = () => {
                     </article>
                 </div>
                 <form action="">
-                    <input type="text" name='name' value={contactMessage.name} placeholder='Your Full Name' required onChange={(e) => handleChange(e)} />
-                    <input type="email" name='email' value={contactMessage.email} placeholder=' Your Email' required onChange={(e) => handleChange(e)} />
-                    <textarea name="message" placeholder='Your Message' value={contactMessage.message} rows={7} required onChange={(e) => handleChange(e)}></textarea>
+                    <input type="text" name='name' className={(errorClass == 'name' && !contactMessage.name) ? 'error' : ''} value={contactMessage.name} placeholder='Your Full Name' required onChange={(e) => handleChange(e)} />
+                    <input type="email" name='email' className={(errorClass == 'email' && !contactMessage.email) ? 'error' : ''} value={contactMessage.email} placeholder=' Your Email' required onChange={(e) => handleChange(e)} />
+                    <textarea name="message" className={(errorClass == 'message' && !contactMessage.message) ? 'error' : ''} placeholder='Your Message' value={contactMessage.message} rows={7} required onChange={(e) => handleChange(e)}></textarea>
                     <button type='submit' className='btn btn-primary' onClick={(e) => onSubmit(e)}>Send Message</button>
                 </form>
             </div>
